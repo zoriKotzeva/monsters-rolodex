0ee6160194eafbac0a4a766575b265f265af0a5b
ZoriKotzeva <zori.kotzeva@gmial.com>
2019-08-21 20:50:18 -0400

    Updates

---
+++ precache-manifest.bb15ecf966a606d88c2c3569037ada31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff7ec770336a898d240c640fa535c0d9",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "91c63dc3bb7bb50b1890",
+    "url": "/monsters-rolodex/static/css/main.9d331337.chunk.css"
+  },
+  {
+    "revision": "d4812ec32942340443a5",
+    "url": "/monsters-rolodex/static/js/2.fd4f73a2.chunk.js"
+  },
+  {
+    "revision": "91c63dc3bb7bb50b1890",
+    "url": "/monsters-rolodex/static/js/main.4e380ecd.chunk.js"
+  },
+  {
+    "revision": "9ea8f628b70732c26c29",
+    "url": "/monsters-rolodex/static/js/runtime~main.45524bec.js"
+  }
+]);
